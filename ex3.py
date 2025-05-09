e49b8280991ae895ba406b284b55f6bc9806b369
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-05-09 02:31:10 +0100

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,11 @@
+def calcul_moy(n1=10,n2=12):
+   moy=(c1*n1+c2*n2)/(c1+c2)  
+ 
+c1=float(input("entre le premier coefficient:"))
+c2=float(input("entre le second coefficient:"))  
+n1=10
+n2=12
+
+print(f"moy est:,{calcul_moy}")
+calcul_moy(n1=10,n2=12)
+calcul_moy(n1=10,n2=12)
